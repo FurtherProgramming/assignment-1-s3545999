bc083c9f151a9127caaa6701a9f999d67339a3ae
s3545999 <80495455+s3545999@users.noreply.github.com>
2021-03-15 21:55:09 +1100

    Add files via upload

--- BookStore.java
+++ BookStore.java
@@ -1,7 +1,82 @@
+import java.util.Scanner;
+
 public class BookStore 
 {
     
+    public static void runMenu() 
+    {
+        String menu = "";
+
+        menu += "Welcome to Daintree!\n";
+        menu += "Choose an option:\n";
+        menu += "1. Add a book to shopping cart\n";
+        menu += "2. View shopping cart\n";
+        menu += "3. Remove a book from shopping cart\n";
+        menu += "4. Checkout\n";
+        menu += "5. List all books\n";
+        menu += "0. Quit\n";
+        menu += "Please make a selection: ";
+
+        System.out.print(menu);
+    }
+    
     public void run()
+    {
+        Book books[] = fillBookstore();
+        Scanner scanner = new Scanner(System.in);
+
+        // Loop to continue offering the menu to choose another option
+        while(true) 
+        {   
+            runMenu();
+            int menuChoice = scanner.nextInt();
+            scanner.nextLine(); // To take away the next line so it waits for input
+
+            if (menuChoice == 1)
+            {
+                System.out.print("\nEnter title to search for: ");
+
+                String bookTitle = scanner.nextLine();
+
+                int match = checkBook(books, bookTitle);
+
+                addToCart(books, match);
+            }
+            else if (menuChoice == 2)
+            {
+                // View shopping Cart
+            }
+            else if (menuChoice == 3)
+            {
+                // Remove book from cart
+            }
+            else if (menuChoice == 4)
+            {
+                // Checkout
+            }
+            else if (menuChoice == 5)
+            {
+                // List Books
+            }
+            else if (menuChoice == 0)
+            {
+                scanner.close();
+                System.exit(0);
+            }
+            else
+            {
+                System.out.print("\nSorry that is not a valid option!\n\n");
+            }
+
+        }
+        
+    }
+
+    /**
+        Creates the predetermined array of books which fill the store
+        @Returns the array of book objects
+    **/
+    public Book[] fillBookstore()
     {
         final int NUMBEROFBOOKS = 5;
         Book[] books = new Book[NUMBEROFBOOKS];
@@ -14,7 +89,39 @@ public class BookStore
         
         for(int i=0; i < NUMBEROFBOOKS; i++)
         {
-            book[i] = new Book(titles[i], authors[i],numberOfItems[i], ebookAvailable[i]);
+            books[i] = new Book(titles[i], authors[i],numberOfItems[i], ebookAvailable[i]);
         }
+
+        return books;
+    }
+
+
+    public static int checkBook(Book[] books, String bookToCheck) 
+    {   
+        bookToCheck = bookToCheck.toLowerCase();
+        int matchedBook = -1;
+
+        for (int i = 0; i < 5 && matchedBook == -1; i++)
+        {
+            String arrayTitle = books[i].getTitle().toLowerCase();
+
+            if (arrayTitle.startsWith(bookToCheck))
+            {
+                matchedBook = i;
+            }
+        }
+    
+        return matchedBook;
+    }
+
+    public void addToCart(Book[] books, int matchedBook)
+    {
+        if(matchedBook != -1)
+        {
+            System.out.print("\nThe following title is a match: \n");
+            System.out.printf("%n%-35s%-20s%-15s%-15s%n", "Title", "Author", "Availabilty", "eBook Availability");
+            System.out.printf("%-35s%-20s%-15d%-15b%n", books[matchedBook].getTitle(), books[matchedBook].getAuthor(), books[matchedBook].getNumOfCopies(), books[matchedBook].getEbookAvailability());
+        }
+        
     }
 }
--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Main
 {
-    public static void runMenu() 
+    public void runMenu() 
     {
 
         String menu = "";
