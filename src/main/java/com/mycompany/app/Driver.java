fcd9b8d3bf06725ba01b5dca41045b45b4251588
Coding Rooms <git@codingrooms.io>
2021-03-26 23:46:22 +0000

    testing .git

--- src/main/java/com/mycompany/app/Driver.java
+++ src/main/java/com/mycompany/app/Driver.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 // This is my driver
 public class Driver
 {
+    // This comment is for testing Git
     public static void main(String[] args) 
     {
         new BookStore().run();
